7ba183f20f9fc36058817e0b836f6f1e3eb13dc7
birama <biramasidibe794@gmail.com>
2025-10-09 14:17:24 +0200

    code corriger

--- gestion_des_presences/views.py
+++ gestion_des_presences/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def accueil(request):
-    return render(request, 'presences/accueil.html')
+    return render(request, 'presence/accueil.html')
