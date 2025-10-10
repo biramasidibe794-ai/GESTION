903c414bb319cffbfb7e3e6444e3272c432080fc
birama <biramasidibe794@gmail.com>
2025-10-10 13:30:14 +0200

    configurer une liste des présences

--- gestion_des_presences/views.py
+++ gestion_des_presences/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Etudiants
 def accueil(request):
     etudiants = [
         {'nom': 'Dupont', 'prenom': 'Jean',},
@@ -7,3 +8,8 @@ def accueil(request):
         {'nom': 'Durand', 'prenom': 'Pierre'},
     ]
     return render(request, 'presence/accueil.html' , {'etudiants': etudiants})
+
+
+def liste_etudiants(request):
+    etudiants= Etudiants.objects.all()
+    return render(request, 'presence/liste_etudiants.html', {'etudiants': etudiants})
