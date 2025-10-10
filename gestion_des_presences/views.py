4fccdefcdf1ee33937e417f5e21a877755ac11df
birama <biramasidibe794@gmail.com>
2025-10-10 13:12:13 +0200

    configurer bootstrap

--- gestion_des_presences/views.py
+++ gestion_des_presences/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 def accueil(request):
-    etudiants = [
-        {'nom': 'Dupont', 'prenom': 'Jean',},
-        {'nom': 'Martin', 'prenom': 'Claire'},
-        {'nom': 'Durand', 'prenom': 'Pierre'},
-    ]
-    return render(request, 'presence/accueil.html' , {'etudiants': etudiants})
+    
+    return render(request, 'accueil.html')
