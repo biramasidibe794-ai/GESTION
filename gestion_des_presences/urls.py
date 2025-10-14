22fbaee41bd24aeccd928a4e773b7f0ec85c596c
birama <biramasidibe794@gmail.com>
2025-10-14 11:07:42 +0200

    .

--- gestion_des_presences/urls.py
+++ gestion_des_presences/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.accueil, name='accueil'),
+    path('etudiants/', views.liste_etudiants, name='liste_etudiants'),#nouvelle route
+    path('annees/', views.annees_list, name='annees_list'),
+    path('annees/create/', views.annees_create, name='annees_create'),
+    path('annees/<int:pk>/update/', views.annees_update, name='annees_update'),
+    path('annees/<int:pk>/delete/', views.annees_delete, name='annees_delete'),
 ]
